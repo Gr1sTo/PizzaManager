ec457983bc70d94b2642c2661386b29b6d1e9039
ArHudzii <art.gudziy@gmail.com>
2023-12-13 17:08:09 +0200

    Added test about pizza

---
+++ PizzaManagerTests/Hudzii/Class3.cs
@@ -0,0 +1,29 @@
+﻿namespace PizzaManagerTests.Test1
+{
+    public class UnitTest3
+    {
+        
+        //перевірка чи успішно додано та видалено піцу з замовлення
+        [Fact]
+        public void RemovePizzaFromOrder_NullPizza_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var orderingSystem = new PizzaOrderingSystem();
+            Pizza nullPizza = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => orderingSystem.RemovePizzaFromOrder(nullPizza));
+        }
+        [Fact]
+        
+        public void AddPizzaToOrder_NullPizza_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var orderingSystem = new PizzaOrderingSystem();
+            Pizza nullPizza = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => orderingSystem.AddPizzaToOrder(nullPizza));
+        }
+    }
+}
